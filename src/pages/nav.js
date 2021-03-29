d871ad05b743771df8e30ef7f5ab98ec1778e3f0
Wilhelmina Tetteh <yemoleytetteh333@gmail.com>
2021-03-29 10:45:55 -0700

    feature: Nav popup on screen size 320 and color change on scroll

---
+++ src/pages/nav.js
@@ -0,0 +1,26 @@
+//Open Navigation Popup
+const openNav = document.querySelector(".navbar__button");
+const navPopup = document.querySelector(".navbar__popup");
+
+openNav.addEventListener("click", (event) => {
+  navPopup.classList.toggle("navbar_open");
+  event.target.classList.toggle("navbar__button_active");
+});
+
+const navContainer = document.querySelector(".navbar__popup-container");
+const navLink = document.querySelectorAll(".navbar__popup-link");
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+  if (window.scrollY > header.offsetHeight - 20) {
+    navContainer.classList.add("navbar__popup-container-light");
+    // CHANGE COLOR FOR EACH LINK
+    navLink.forEach(function (link) {
+      link.classList.add("navbar__popup-link-light");
+    });
+  } else {
+    navContainer.classList.remove("navbar__popup-container-light");
+    navLink.forEach(function (link) {
+      link.classList.remove("navbar__popup-link-light");
+    });
+  }
+});
